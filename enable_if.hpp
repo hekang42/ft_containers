f3e9a664568ccaf3830abe56545cc96b03d4f8dd
Hee young Kang <hekang@student.42seoul.kr>
2021-09-27 20:29:17 +0900

    add enable_if

--- enable_if.hpp
+++ enable_if.hpp
@@ -1,8 +1,12 @@
 #ifndef ENABLE_IF_HPP
 #define ENABLE_IF_HPP
-template < bool Cond, class T = void >
-struct enable_if {
-	typedef T type;
-};
+namespace ft {
+template < bool, typename T = void >
+struct enable_if {};
 
+template < typename T >
+struct enable_if< true, T > {
+  typedef T type;
+};
+}  // namespace fd
 #endif
---
+++ is_integral.hpp
@@ -0,0 +1,13 @@
+#if !defined(IS_INTEGRAL_HPP)
+# define IS_INTEGRAL_HPP>
+class Is_integral
+{
+private:
+	/* data */
+public:
+	Is_integral(/* args*/);
+	Is_integral(const test &other){};
+	Is_integral &operator=(const test &other){};
+	~Is_integral();
+};
+#endif // IS_INTEGRAL_HPP>
